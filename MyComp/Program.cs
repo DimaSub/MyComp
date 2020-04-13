26512eb44009d6cb0a88d96f43916a3360ab771f
Dima Subotin <57070189+DimaSub@users.noreply.github.com>
2020-04-13 23:48:31 +0300

    Project start

---
+++ MyComp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyComp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
